a76cbd0a1dd32fa840fc07667295c35451735fe8
nevermage <maksim14032002@gmail.com>
2021-10-01 17:31:28 +0300

    1

---
+++ welcome.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>welcome</title>
+</head>
+<body style="">
+<style>
+    body{
+        background: #373947;
+        color: #f5f5f5;
+        font-family: calibri;
+    }
+    table{
+        color: #000;
+    }
+</style>
+<h1>welcome</h1>
+<form method="post">
+    <input type="submit" action="<?php header('Location: test1.php');?>" value="logout">
+</form>>
+</body>
+</html>
+
+<?php
+session_start();
+$e='123';
+$p='321';
+if (isset($_POST['email']) == 1) {
+    if ($_POST['email'] == $e && $_POST['password'] == $p) {
+        $_SESSION['u'] = $_POST['email'];
+    }
+}
+else{
+    header('Location: test1.php');
+}
+
+?>
