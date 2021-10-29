80b72d62026cab42014892469fa5dcb830760d1d
nevermage <maksim14032002@gmail.com>
2021-10-29 20:29:54 +0300

    hw2

--- pr2.php
+++ pr2.php
@@ -27,21 +27,50 @@
     //         c: 2,
     //     },
     // };
+    const f1 = () => {
+        throw Error('zxc');
+    }
+
+    const deepClone = obj => {
+        if (obj === null) return null;
+        // Создаем поверхностный клона оригинала.
+        let clone = Object.assign({}, obj);
+
+        // Определяем, какие пары ключ-значение
+        // необходимо глубоко клонировать.
+        Object.keys(clone).forEach(
+            key =>
+                (clone[key] =
+                    typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key])
+        );
+
+        // Проверяем является ли obj массивом и не пустой ли он.
+        return Array.isArray(obj) && obj.length
+            // Если obj массив и он не пуст, тогда
+            // указываем объекту clone длину исходного массива что бы
+            // конвертировать clone в массив и вернуть его.
+            ? (clone.length = obj.length) && Array.from(clone)
+            // Если obj пустой массив,
+            : Array.isArray(obj)
+                // то возвращаем его
+                ? Array.from(obj)
+                // в других случаях obj это объект и мы возвращаем копию clone.
+                : clone;
+    };
+
     const cpy = (obj) => {
         if (obj instanceof Error) {
             return Error(obj.message);
         }
-        return (JSON.parse(JSON.stringify(obj)));
+        return deepClone(obj);
     }
-    const f1 = () => {
-        throw Error('zxc');
-    }
+
     try {
         f1();
     } catch (e) {
+        nobj = cpy(a);
         console.log(e);
         console.log("after copy");
-        nobj = cpy(e);
         console.log(nobj);
     }
 
